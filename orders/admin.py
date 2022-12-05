84c972d0f85b2aab205de9d7ddca843b24b3af7d
Hatef Barin <amir.barin76@gmail.com>
2022-12-05 17:02:11 +0330

    Create an OderItem's model and setup Admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItem
+from jalali_date.admin import ModelAdminJalaliMixin
+
+
+class OrderItemInLine(admin.TabularInline):
+    model = OrderItem
+    fields = ['order', 'product', 'quantity', 'price', ]
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
+    list_display = ['user', 'fist_name', 'last_name', 'datetime_created', 'is_paid', ]
+    inlines = [
+        OrderItemInLine,
+    ]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'product', 'quantity', 'price', ]
--- orders/models.py
+++ orders/models.py
@@ -12,5 +12,22 @@ class Order(models.Model):
     phone_number = models.CharField(max_length=15)
     address = models.CharField(max_length=800)
 
+    order_notes = models.CharField(max_length=200, blank=True)
+
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Order {self.id}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey('products.Product',
+                                on_delete=models.CASCADE,
+                                related_name='order_items')
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"OrderItem {self.id}: {self.product} x {self.quantity} (price:{self.price})"
