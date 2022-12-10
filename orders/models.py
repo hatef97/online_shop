978243d61f9c5dfd51913589773e31fc5b6a4495
Hatef Barin <amir.barin76@gmail.com>
2022-12-10 22:35:57 +0330

    Add forms for Orders

--- orders/models.py
+++ orders/models.py
@@ -1,21 +1,22 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 from config.settings import AUTH_USER_MODEL
 
 
 class Order(models.Model):
-    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
-    is_paid = models.BooleanField(default=False)
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name=_('User'))
+    is_paid = models.BooleanField(_('Is Paid'), default=False)
 
-    fist_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=15)
-    address = models.CharField(max_length=800)
+    fist_name = models.CharField(_('First Name'), max_length=100)
+    last_name = models.CharField(_('Last Name'), max_length=100)
+    phone_number = models.CharField(_('Phone Number'), max_length=15)
+    address = models.CharField(_('Address'), max_length=800)
 
-    order_notes = models.CharField(max_length=200, blank=True)
+    order_notes = models.CharField(_('Order Notes'), max_length=200, blank=True)
 
-    datetime_created = models.DateTimeField(auto_now_add=True)
-    datetime_modified = models.DateTimeField(auto_now=True)
+    datetime_created = models.DateTimeField(_('Datetime Created'), auto_now_add=True)
+    datetime_modified = models.DateTimeField(_('Datetime Modified'), auto_now=True)
 
     def __str__(self):
         return f"Order {self.id}"
